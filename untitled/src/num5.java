c4cdc50e3306d70cb0d0813f3884a9ccb6d9a958
pks118 <arbuz228kir@gmail.com>
2025-09-14 15:38:27 +0300

    Переделал задачу №5

--- untitled/src/num5.java
+++ untitled/src/num5.java
@@ -2,18 +2,13 @@ public class num5 {
     public static void main(String[] args) {
         String str = "hello world and always come back";
         int step = 0;
-        for(int i =0; i<str.length();i++){
+        for (int i = 0; i <= str.length(); i++) {
             step++;
-            if(str.charAt(i)==' ' )
-            {
-                System.out.println(str.substring(i-(step-1),i));
-                step=0;
+            if (i == str.length() || str.charAt(i) == ' ') {
+                System.out.println(str.substring(i - (step - 1), i));
+                step = 0;
             }
         }
-        for (int i = str.length() - 1; i != 0; i--)
-            if (str.charAt(i) == ' ') {
-                System.out.println(str.substring(i+1));
-                break;
-            }
+
     }
 }
--- untitled/src/num6.java
+++ untitled/src/num6.java
@@ -3,14 +3,14 @@ public class num6 {
         int shiftCh = 10;
         String str = "hello world and always come back", str2="";
         for (char ch : str.toCharArray()){
-            System.out.println(ch + " " + (char)(ch-10));
-            str2+= (char)(ch-10);
+            System.out.println(ch + " " + (char)(ch-shiftCh));
+            str2+= (char)(ch-shiftCh);
         }
         System.out.println(str2);
         str="";
         for (char ch : str2.toCharArray()){
-            System.out.println(ch + " " + (char)(ch+10));
-            str+= (char)(ch+10);
+            System.out.println(ch + " " + (char)(ch+shiftCh));
+            str+= (char)(ch+shiftCh);
         }
         System.out.println(str);
     }
