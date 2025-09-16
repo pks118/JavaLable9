28af6407f952fe03a78db515f04d9e36a71c9539
pks118 <arbuz228kir@gmail.com>
2025-09-16 20:29:26 +0300

    Необходимо доделать номер 9 (в целом он готов, но нужно сделать красивый вывод - он кривой)
    Signed-off-by: pks118 <arbuz228kir@gmail.com>

--- untitled/src/num10.java
+++ untitled/src/num10.java
@@ -1,6 +1,13 @@
 public class num10 {
     public static void main(String[] args) {
-        String str = "12345";
-        System.out.println(str.substring(0,3));
+        String str = "12.13.3000";
+        System.out.println(checkingEnteredDay(str.substring(6)));
+    }
+    private static int checkingEnteredDay (String nums)
+    {
+        int num = 0;
+        for (int i = nums.length()-1; i>=0; i--)
+            num += (nums.charAt(i) - 48) * ((int) Math.pow(10, (nums.length()-1)-i));
+        return num;
     }
 }
--- untitled/src/num9.java
+++ untitled/src/num9.java
@@ -9,14 +9,15 @@ public class num9 {
         boolean flag = true;
         int stage = 0;
 
+
         for (int i = 0; i < date.length(); i++) {
             if ((!(date.charAt(i) >= '0' && date.charAt(i) <= '9')) && date.charAt(i) != '.') {
                 error = "Ошибка: \"" + date.charAt(i) + "\" не допустимый формат!";
                 flag = false;
                 break;
             } else if ((date.charAt(i) == '.')) {
-                if (i != 2 && stage == 0) {
-                    error = "Ошибка: дата \"" + date.substring(0, i) + "\" введена не корректно!";
+                if (i != 2 && stage == 0) {//проверяю стоит ли точка на своём месте
+                    error = "Ошибка: день \"" + date.substring(0, i) + "\" введена не корректно!";
                     flag = false;
                     break;
                 } else if (i != 5 && stage == 1) {
@@ -27,19 +28,36 @@ public class num9 {
                 stage++;
             }
         }
-        if (flag && date.length() == 10) {
-            if (checkingEnteredDay(date.substring(0,2))<=31)
-                System.out.println(date.substring(0, 2));
-            else
-                System.out.println("Ошибка: некорректная дата: \"" + date.substring(0, 2) + "\" !");
-            System.out.println(error);
 
-            //System.out.println(date.substring(3, 5));
-            //System.out.println(date.substring(6));
+
+        if (flag && date.length() == 10) {//проверяю на наличие ошибки и вышел ли пользователь за допустимый формат
+            if (checkingEnteredNumber(date.substring(0,2))>31) {//проверка даты и.т.д
+                flag = false;
+                error = "Ошибка: некорректная дата: \"" + date.substring(0, 2) + "\" !";
+            }
+            if (checkingEnteredNumber(date.substring(3,5))>12 || checkingEnteredNumber(date.substring(3,5))<1) {
+                flag = false;
+                error = "Ошибка: некорректный месяц: \"" + date.substring(3, 5) + "\" !";
+            }
+
+            if (checkingEnteredNumber(date.substring(6))<1925 || checkingEnteredNumber(date.substring(6))>2025) {
+                flag = false;
+                error = "Ошибка: некорректный год: \"" + date.substring(6) + "\" !";
+            }
         } else System.out.println(error);
+
+
+
+        if (flag)//вывод
+            System.out.println(date.substring(0,2)+"\n"+date.substring(3, 5)+"\n"+date.substring(6));
+        else
+            System.out.println(error);
     }
-    private static int checkingEnteredDay (String num)
+    private static int checkingEnteredNumber (String nums)//конвертирую из строки в число
     {
-        return Integer.parseInt(num);
+        int num = 0;
+        for (int i = nums.length()-1; i>=0; i--)
+            num += (nums.charAt(i) - 48) * ((int) Math.pow(10, (nums.length()-1)-i));
+        return num;
     }
 }
